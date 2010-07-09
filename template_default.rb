eb7c2f837e6b82bb3ecdf6d7ec193e96b245a5eb
Brad Werth <bradwerth@gamil.com>
2010-07-09 00:18:10 -0500

    changed template root to github

--- template_default.rb
+++ template_default.rb
@@ -1,5 +1,7 @@
 
-TEMPLATE_ROOT = "c:\\templates\\"
-INCLUDED_TEMPLATES = %w( modify_default_files heroku git )
+TEMPLATE_ROOT = "http://github.com/brad-werth/rails-templates/raw/master/"
+INCLUDED_TEMPLATES = %w( modify_default_files
+                         heroku
+                         git )
 
 INCLUDED_TEMPLATES.each { |template| apply "#{ TEMPLATE_ROOT }#{ template }.rb"  }
