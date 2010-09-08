198d065f0c7a5415af27aa916b1cf6c3a7908d1a
brad-werth <bradwerth@gmail.com>
2010-09-08 00:39:25 -0500

    gems

--- template_default.rb
+++ template_default.rb
@@ -2,6 +2,7 @@
 TEMPLATE_ROOT = "http://github.com/brad-werth/rails-templates/raw/master/"
 INCLUDED_TEMPLATES = %w( git
                          modify_default_files
-                         heroku )
+                         heroku
+                         add_entries_to_gemfile )
 
 INCLUDED_TEMPLATES.each { |template| apply "#{ TEMPLATE_ROOT }#{ template }.rb"  }
